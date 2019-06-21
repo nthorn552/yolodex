6e6acf5889199fb8977d97dc3121ae0fc060ac70
Nathaniel Thorn <nate@arcsource.com>
2019-06-21 16:07:44 -0700

    WIP: express server setup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,6 +4,8 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
+import { Button } from "@material-ui/core";
+
 const App = () => (
   <CssBaseline>
     <Container maxWidth="lg">
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [ "*", ".js", ".jsx", ".ts", ".tsx" ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: ['awesome-typescript-loader']
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 3000,
-        historyApiFallback: {
-            index: 'index.html'
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html')
-        })
+  entry: "./src/client/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ["awesome-typescript-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
     ]
-}
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3000,
+    historyApiFallback: {
+      index: "index.html"
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/client/index.html")
+    })
+  ]
+};
