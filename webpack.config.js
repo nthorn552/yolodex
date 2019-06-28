2d08072737283405d8e0c4dc46a60f2928f902bd
Nathaniel Thorn <nate@arcsource.com>
2019-06-28 16:33:18 -0700

    clean up package

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
