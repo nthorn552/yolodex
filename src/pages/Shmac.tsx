38fe3df87b42bdb88962242d5fbe0432f1eed97d
Nathaniel Thorn <nthorn@riskalyze.com>
2019-07-10 09:55:34 -0700

    and finally typed out the styles

--- src/pages/Shmac.tsx
+++ src/pages/Shmac.tsx
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 import { Device } from "../reducers/devices/types";
 
+import { makeStyles, Theme, createStyles, WithStyles, withStyles } from "@material-ui/core/styles";
 import { Paper, Container } from "@material-ui/core";
+
+const styles = ({ spacing }: Theme) => createStyles({
+  pageWrapper: {
+    paddingTop: spacing(2)
+  }
+});
 
 type State = {
   targetDevice: Device;
@@ -12,12 +19,15 @@ const initialState: State = {
   targetDevice: null
 };
 
-class ShmacPage extends Component<object, State> {
-  readonly state: State = initialState;
+class ShmacPage extends Component<WithStyles<typeof styles>> {
 
   render = () => {
-    return <Paper>test</Paper>;
+    return (
+      <Container className={this.props.classes.pageWrapper}>
+        <Paper>test</Paper>
+      </Container>
+    );
   };
 }
 
-export default ShmacPage;
+export default withStyles(styles)(ShmacPage);
