d14e90fc6f04165825ffbe701ebf45ecf3197d63
Nathaniel Thorn <nthorn@riskalyze.com>
2019-08-01 20:23:08 -0700

    try this package optimization + remove garbage

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -14,12 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: ["awesome-typescript-loader"]
-      },
-      {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
       }
